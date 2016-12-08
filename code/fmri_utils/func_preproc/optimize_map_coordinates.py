8f92d3dc00e5ec7d3f75caeb5bb703a170592109
Jacob Miller <jcbmiller94@gmail.com>
2016-12-07 23:10:56 -0800

    Added new framework to capture rigid body transformations between volumes through coordinate mapping (and testing)

---
+++ code/fmri_utils/func_preproc/optimize_map_coordinates.py
@@ -0,0 +1,144 @@
+# - compatibility with Python 2
+from __future__ import print_function  # print('me') instead of print 'me'
+from __future__ import division  # 1/2 == 0.5, not 0
+# - import common modules
+import numpy as np  # the Python array package
+import numpy.linalg as npl
+import matplotlib.pyplot as plt  # the Python plotting package
+import scipy.ndimage as snd
+from scipy.optimize import fmin_powell
+import nibabel as nib
+
+# import of rotations code
+#from rotations import x_rotmat, y_rotmat, z_rotmat
+from .rotations import x_rotmat, y_rotmat, z_rotmat
+
+# Mismatch function between two volumes
+def correl_mismatch(ref_vol, vol1):
+    """ Negative correlation between the two images, flattened to 1D
+
+    Parameters
+    ----------
+    ref_vol: array shape (I, J, K)
+        array with data from the reference volume
+
+    vol1: array shape (I, J, K)
+        array with data from the volume being transformed to the reference
+
+    Returns
+    -------
+    correl: float
+        correlation coefficient between the reference and transformed volumes
+
+    """
+    correl = np.corrcoef(ref_vol.ravel(), vol1.ravel())[0, 1]
+    return -correl
+
+# Resampling function for any given volume and the reference (ref_vol)
+def apply_coord_mapping(RB_params, ref_vol, vol1, ref_vol_affine):
+    """ Resample `ref_vol` to 'vol1' after applying a moving affine based on RB_params
+
+    Parameters
+    ----------
+    RB_params: array shape (6,)
+        array with rigid body transfomations (3 translations, 3 rotations) to be applied
+
+    ref_vol: array shape (I, J, K)
+        array with data from the reference volume
+
+    vol1: array shape (I, J, K)
+        array with data from the volume being transformed to the reference
+
+    ref_vol_affine: array shape (4, 4)
+        image affine mapping for the reference volume
+
+    Returns
+    -------
+    resampled_vol1: arra shape (I, J, K)
+        vol1 resampled according to the rigid body parameters
+
+    """
+    # get rotation value sin each direction and make a rotation matrix
+    r_x, r_y, r_z = RB_params[3:]
+    rot_mat = z_rotmat(r_z).dot(y_rotmat(r_y).dot(x_rotmat(r_x)))
+    # get translation values in each direciton
+    trans = np.array(RB_params[0:3])
+    moving_affine = nib.affines.from_matvec(rot_mat, trans)
+
+    # affine maping bet the moving affine and the affine of the reference volume
+    vol1_to_ref_vol = npl.inv(moving_affine).dot(ref_vol_affine)
+
+    # get shape of reference volume
+    I, J, K = ref_vol.shape
+    # create a mesh coordinate grid
+    i_vals, j_vals, k_vals = np.meshgrid(range(I), range(J), range(K), indexing='ij')
+    in_vox_coords = np.array([i_vals, j_vals, k_vals])
+
+    ### Add SPM random noise algorithm here to coordinates !!
+
+    coords_last = in_vox_coords.transpose(1, 2, 3, 0)
+    vol1_vox_coords = nib.affines.apply_affine(moving_affine, coords_last)
+    coords_first_again = vol1_vox_coords.transpose(3, 0, 1, 2)
+    # Resample using map_coordinates
+    resampled_vol1 = snd.map_coordinates(vol1, coords_first_again)
+
+    return resampled_vol1
+
+
+# Cost function using xyz translations and the first volume with the given volume
+def cost_at_xyz(RB_params, ref_vol, vol1, ref_vol_affine):
+    """ Give cost function value at rigid body transformation values
+
+        Parameters
+        ----------
+        RB_params: array shape (6,)
+            array with rigid body transfomations (3 translations, 3 rotations) to be applied
+
+        ref_vol: array shape (I, J, K)
+            array with data from the reference volume
+
+        vol1: array shape (I, J, K)
+            array with data from the volume being transformed to the reference
+
+        ref_vol_affine: array shape (4, 4)
+            image affine mapping for the reference volume
+
+        Returns
+        -------
+        correl_mismatch: function
+            reference to the mismatch function bewteen the resampled and reference volumes
+    """
+    # Resample vol1 for x,y,z translations and rotations and return the mismatch
+    #  function value for the given rigid body parameters
+    resampled_vol = apply_coord_mapping(RB_params, ref_vol, vol1, ref_vol_affine)
+    return correl_mismatch(resampled_vol, ref_vol)
+
+# Optimization function to obtain best motion parameters and shifted volume
+def optimize_map_vol(ref_vol, vol1, ref_vol_affine):
+    """ Optimize the transofmation of vol1 to best match ref_vol
+
+        Parameters
+        ----------
+        ref_vol: array shape (I, J, K)
+            array with data from the reference volume
+
+        vol1: array shape (I, J, K)
+            array with data from the volume being transformed to the reference
+
+        ref_vol_affine: array shape (4, 4)
+            image affine mapping for the reference volume
+
+        Returns
+        -------
+        optimized_vol1: array shape (I, J, K)
+            rnew volume with optimized RB parameters applied
+
+        best_params: array shape (6,)
+            array of 3 translations and rotations to best match vol1 to reference
+    """
+    # best guess for rigid body transformations (3 translations, 3 rotations) - starting with 0
+    RB_params_guess = [0, 0, 0, 0, 0, 0]
+    best_params = fmin_powell(cost_at_xyz, RB_params_guess, args = (ref_vol, vol1, ref_vol_affine))
+    optimized_vol1 = apply_coord_mapping(best_params, ref_vol, vol1, ref_vol_affine)
+
+    return optimized_vol1, best_params
---
+++ code/fmri_utils/tests/test_coord_mapping.py
@@ -0,0 +1,61 @@
+""" py.test test for mapping coordinates code
+
+Run with:
+
+    py.test fmri_utils/tests/test_coord_mapping.py
+"""
+# import common modules
+import numpy as np
+import numpy.linalg as npl
+from numpy.testing import assert_almost_equal
+import scipy.ndimage as snd
+from scipy.optimize import fmin_powell
+import nibabel as nib
+
+from os.path import dirname, join as pjoin
+MY_DIR = dirname(__file__)
+
+EXAMPLE_FILENAME = 'ds114_sub009_t2r1.nii'
+
+#from fmri_utils.func_preproc.optimize_rotations import optimize_rot_vol
+#: Check import of rotations code
+from fmri_utils.func_preproc.rotations import x_rotmat, y_rotmat, z_rotmat
+
+from fmri_utils.func_preproc.optimize_map_coordinates import optimize_map_vol
+
+def test_optimize_map_vol():
+    # Test optimization of rotations between two volumes
+    print(MY_DIR)
+    example_path = pjoin(MY_DIR, EXAMPLE_FILENAME)
+    #expected_values = np.loadtxt(pjoin(MY_DIR, 'global_signals.txt'))
+
+    #img = nib.load('ds114_sub009_t2r1.nii')
+    img = nib.load(example_path)
+    data = img.get_data()
+    vol0 = data[..., 4]
+    vol1 = data[..., 5]
+
+    #vol1_affine = img.affine
+
+    # add an intentionally rotated volume by X, Y, Z
+    X = x_rotmat(0.5)
+    #- * radians around the y axis, then
+    Y = y_rotmat(0.07)
+    #- * radians around the z axis.
+    Z = z_rotmat(-0.3)
+    rotations = np.array([0.5, 0.07, -0.3])
+    #- Mutiply matrices together to get matrix describing all 3 rotations
+    M = Z.dot(Y.dot(X))
+
+    translations = np.array([-1.,-3.,-2])
+    test_params = np.append(translations, rotations)
+
+    transformed_vol1 = snd.affine_transform(vol1, M, translations, order = 1)
+
+    # test to see whether to optimization of resampling function
+    #  indeed captures a very similar volume between the transformed and original
+
+    resampled_vol1, best_params = optimize_map_vol(vol1, transformed_vol1, img.affine)
+    assert_almost_equal(best_params, test_params, decimal = 4)
+
+    return
