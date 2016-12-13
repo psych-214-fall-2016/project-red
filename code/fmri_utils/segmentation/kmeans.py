0197586912d0f0a69d6ba69bfa5e846540922a02
Christine Tseng <ctseng12@mgmail.com>
2016-12-12 16:14:22 -0800

    Added documentation

--- code/fmri_utils/segmentation/kmeans.py
+++ code/fmri_utils/segmentation/kmeans.py
@@ -1,13 +1,61 @@
+"""
+Segmentation using k-means clustering. Each pixel is assigned to the cluster
+with the closest mean.
+
+@author: Christine Tseng
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import nibabel as nib
 
 def should_go(centers, old_centers, iteration, max_iter):
+    """
+    Returns whether kmeans should run for another iteration.
+
+    Input
+    -----
+    centers : array (k,)
+        Contains the means for each cluster after the newest iteration.
+
+    old_centers : array (k,)
+        Contains the means for each cluster from the previous iteration.
+
+    iteration : int
+        Current iteration number.
+
+    max_iter : int
+        Maximum number of iterations.
+
+    Output
+    ------
+    - : boolean
+        True if kmeans should centers != old_centers and iteration < max_iter.
+        False otherwise.
+    """
+
     if iteration > max_iter:
         return False
     return not np.allclose(centers, old_centers)
 
 def get_labels(x, centers):
+    """
+    Returns labels/cluster assignments for each pixel given the cluster means.
+    Each pixel is assigned to the cluster that has the closest mean.
+
+    Input
+    -----
+    x : array (n,)
+        Input data.
+
+    centers : array (k,)
+        Contains the means for each cluster.
+
+    Output
+    ------
+    labels : int array (n,)
+        Contains the cluster assignment for each pixel.
+    """
     labels = np.zeros(x.shape)
     for i, xpt in enumerate(x):
         dist = np.abs(centers - xpt)
@@ -15,6 +63,26 @@ def get_labels(x, centers):
     return labels
 
 def get_centers(x, labels, k):
+    """
+    Return means for clusters given cluster assignments for pixels. Each cluster
+    mean is updated to be the mean of the pixels in the cluster.
+
+    Input
+    -----
+    x : array (n,)
+        Input data.
+
+    labels : int array (n,)
+        Contains the cluster assignment for each pixel.
+
+    k : int
+        Number of clusters.
+
+    Output
+    ------
+    centers : array (k,)
+        Contains the updated means for each cluster.
+    """
     centers = np.zeros(k)
     for i in range(k):
         x_cluster = x[labels == i]
@@ -23,6 +91,33 @@ def get_centers(x, labels, k):
     return centers
 
 def kmeans(x, k=4, max_iter=10^4, scale=50):
+    """
+    Run kmeans for data x.
+
+    Input
+    -----
+    x : array (n,)
+        Input data.
+
+    k : int, default=4
+        Number of clusters.
+
+    max_iter : int, default=10^4
+        Maximum number of k-means iterations.
+
+    scale : int, default=50
+        Initial guesses for cluster means are chosen from a uniform distribution
+        between 0 and scale.
+
+    Output
+    ------
+    centers : array (k,)
+        Contains the updated means for each cluster.
+
+    labels : int array (n,)
+        Contains the cluster assignment for each pixel.
+    """
+
     centers = np.random.rand(k) * scale
     old_centers = np.zeros(k)
     iteration = 0
