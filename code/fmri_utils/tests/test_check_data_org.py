a5c87d7a7a28321750d45278e1ec22e1977c1ef1
Zuzanna Balewski <zzbalews@gmail.com>
2016-12-13 10:31:11 -0800

    added error exceptions to improve coverage

--- code/fmri_utils/tests/test_check_data_org.py
+++ code/fmri_utils/tests/test_check_data_org.py
@@ -6,7 +6,8 @@ Run with:
 """
 import os
 from os.path import join as pjoin, dirname
-from fmri_utils.check_data_org import safe_make_dir, make_nii_hashes_nested, validate_data
+from fmri_utils.check_data_org import safe_make_dir, make_nii_hashes_nested, validate_data, main
+import random, string
 
 def test_safe_make_dir():
 
@@ -32,10 +33,7 @@ def test_hash_sample():
 
     # test extraction and generation of hash for MNI template file
     data_directory = pjoin(dirname(__file__))
-    hash_file = "test_data_hashes.txt"
-
-    # make sure file doesn't already exist
-    assert(not os.path.exists(pjoin(data_directory, hash_file))) # dir doesn't exist
+    hash_file = "temp.txt"
 
     # make hash file with existing file
     make_nii_hashes_nested(data_directory, hash_file)
@@ -52,14 +50,44 @@ def test_hash_sample():
     assert(len(sample_idx)==1) # one row with correct file
 
     # rewrite hash_file with only sample_file line
-
     gobj = open(pjoin(data_directory,hash_file), 'wt')
     gobj.write(" ".join(split_lines[sample_idx[0]]))
     gobj.close()
 
-    # Call function to validate data in data directory
     validate_data(data_directory, hash_file)
+
+    # rewrite hash_file with only sample_file line; filename intentionally wrong
+    fake_file = 'temp_img.nii'
+    fake_line = " ".join([split_lines[sample_idx[0]][0], fake_file])
+
+    hobj = open(pjoin(data_directory,hash_file), 'wt')
+    hobj.write(fake_line)
+    hobj.close()
+
+    try:
+        validate_data(data_directory, hash_file)
+    except FileNotFoundError:
+        print('bad file name caught successfully!')
+
+    # rewrite hash_file with only sample_file line; hash intentionally broken
+    fake_hash = "".join([random.choice(string.digits + 'abcdef') for n in range(40)])
+    fake_line = " ".join([fake_hash, split_lines[sample_idx[0]][1]])
+
+    kobj = open(pjoin(data_directory,hash_file), 'wt')
+    kobj.write(fake_line)
+    kobj.close()
+
+    try:
+        validate_data(data_directory, hash_file)
+    except ValueError:
+        print('bad hash caught successfully!')
 
     # remove sample hash_file
     os.remove(pjoin(data_directory,hash_file))
     assert(not os.path.exists(pjoin(data_directory,hash_file))) # dir doesn't exist
+
+def test_main():
+    try:
+        main()
+    except FileNotFoundError:
+        print('testing main; should fail because data missing img files')
