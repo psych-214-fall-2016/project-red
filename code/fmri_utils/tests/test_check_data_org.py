bf94df341123474c6d59fd70687aae0a33d0494d
Zuzanna Balewski <zzbalews@gmail.com>
2016-12-14 12:05:50 -0800

    using tempfile TemporaryDirectory in tests :)

--- code/fmri_utils/tests/test_check_data_org.py
+++ code/fmri_utils/tests/test_check_data_org.py
@@ -6,85 +6,75 @@ Run with:
 """
 import os
 from os.path import join as pjoin, dirname
-from fmri_utils.check_data_org import safe_make_dir, make_nii_hashes_nested, validate_data, main
+from tempfile import TemporaryDirectory
+
 import random, string
 
-def test_safe_make_dir():
+from fmri_utils.check_data_org import make_nii_hashes_nested, validate_data, main
 
-    # check that temporary dir doesn't exit
-    tempdir = 'tempdir_for_test'
-    assert(not os.path.isdir(tempdir))
-
-    # try to make temporary dir
-    try1 = safe_make_dir(tempdir)
-    assert(os.path.isdir(tempdir)) # dir exists
-    assert(try1) # dir created
-
-    # try to make temporary dir again
-    try2 = safe_make_dir(tempdir)
-    assert(os.path.isdir(tempdir)) # dir stil exists
-    assert(not try2) # dir NOT created
-
-    # remove temporary dir
-    os.rmdir(tempdir)
-    assert(not os.path.isdir(tempdir)) # dir doesn't exist
 
 def test_hash_sample():
 
-    # test extraction and generation of hash for MNI template file
-    data_directory = pjoin(dirname(__file__))
-    hash_file = "temp.txt"
+    # move to temp dir so can save files
+    with TemporaryDirectory() as tmpdirname:
+        tempdir = tmpdirname
 
-    # make hash file with existing file
-    make_nii_hashes_nested(data_directory, hash_file)
+        # test extraction and generation of hash for MNI template file
+        data_directory = pjoin(dirname(__file__))
+        temp_file = pjoin(tempdir, 'temp_hash1.txt')
 
-    # check that sample file is in hash_file
-    sample_file = 'ds107_sub012_t1r2_small.nii'
+        # make hash file with existing file
+        make_nii_hashes_nested(data_directory, temp_file)
 
-    fobj = open(pjoin(data_directory,hash_file), 'rt')
-    lines = fobj.readlines()
-    fobj.close()
-    split_lines= [line.split() for line in lines]
-    sample_idx = [i for i in range(len(split_lines)) if split_lines[i][1].find(sample_file)>-1]
+        # check that sample file is in hash_file
+        sample_file = 'ds107_sub012_t1r2_small.nii'
 
-    assert(len(sample_idx)==1) # one row with correct file
+        fobj = open(pjoin(data_directory, temp_file), 'rt')
+        lines = fobj.readlines()
+        fobj.close()
+        split_lines= [line.split() for line in lines]
+        sample_idx = [i for i in range(len(split_lines)) if split_lines[i][1].find(sample_file)>-1]
 
-    # rewrite hash_file with only sample_file line
-    gobj = open(pjoin(data_directory,hash_file), 'wt')
-    gobj.write(" ".join(split_lines[sample_idx[0]]))
-    gobj.close()
+        assert(len(sample_idx)==1) # one row with correct file
 
-    validate_data(data_directory, hash_file)
+        # rewrite hash_file with only sample_file line)
+        temp_file = pjoin(tempdir, 'temp_hash2.txt')
+        gobj = open(pjoin(data_directory, temp_file), 'wt')
+        gobj.write(" ".join(split_lines[sample_idx[0]]))
+        gobj.close()
 
-    # rewrite hash_file with only sample_file line; filename intentionally wrong
-    fake_file = 'temp_img.nii'
-    fake_line = " ".join([split_lines[sample_idx[0]][0], fake_file])
+        validate_data(data_directory, temp_file)
 
-    hobj = open(pjoin(data_directory,hash_file), 'wt')
-    hobj.write(fake_line)
-    hobj.close()
+        # rewrite hash_file with only sample_file line; filename intentionally wrong
+        temp_file = pjoin(tempdir, 'temp_hash3.txt')
+        fake_file = 'temp_img.nii'
+        fake_line = " ".join([split_lines[sample_idx[0]][0], fake_file])
 
-    try:
-        validate_data(data_directory, hash_file)
-    except FileNotFoundError:
-        print('bad file name caught successfully!')
+        hobj = open(pjoin(data_directory, temp_file), 'wt')
+        hobj.write(fake_line)
+        hobj.close()
 
-    # rewrite hash_file with only sample_file line; hash intentionally broken
-    fake_hash = "".join([random.choice(string.digits + 'abcdef') for n in range(40)])
-    fake_line = " ".join([fake_hash, split_lines[sample_idx[0]][1]])
+        try:
+            validate_data(data_directory, temp_file)
+        except FileNotFoundError:
+            print('bad file name caught successfully!')
 
-    kobj = open(pjoin(data_directory,hash_file), 'wt')
-    kobj.write(fake_line)
-    kobj.close()
+        # rewrite hash_file with only sample_file line; hash intentionally broken
+        temp_file = pjoin(tempdir, 'temp_hash4.txt')
+        fake_hash = "".join([random.choice(string.digits + 'abcdef') for n in range(40)])
+        fake_line = " ".join([fake_hash, split_lines[sample_idx[0]][1]])
 
-    try:
-        validate_data(data_directory, hash_file)
-    except ValueError:
-        print('bad hash caught successfully!')
+        kobj = open(pjoin(data_directory, temp_file), 'wt')
+        kobj.write(fake_line)
+        kobj.close()
 
-    # remove sample hash_file
-    os.remove(pjoin(data_directory,hash_file))
-    assert(not os.path.exists(pjoin(data_directory,hash_file))) # dir doesn't exist
+        try:
+            validate_data(data_directory, temp_file)
+        except ValueError:
+            print('bad hash caught successfully!')
+
+    # check that dir deleted
+    assert(not os.path.isdir(tempdir))
 
 def test_main():
     try:
