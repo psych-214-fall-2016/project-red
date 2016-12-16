8297b865e393e83ad49b527cd6f138bd97b6b734
Zuzanna Balewski <zzbalews@gmail.com>
2016-12-15 17:58:54 -0800

    finished quality check (transform template and recover)

--- code/fmri_utils/registration/quality_report.py
+++ code/fmri_utils/registration/quality_report.py
@@ -1,148 +1,100 @@
 """
-quality check for our code, using:
-
-A = template
-T = transformation
-
-B = transformed A with T
-A' = transformed B with inv(T)
-
-T* = found best T with optimization
-
-goal:
-can we recover original transformation on image?
-what is the best MI match after 2 resampling steps?
+quality check for our code: transform template by known translations & rotations
 
 """
+from os.path import dirname, join as pjoin, abspath
+import csv
 
-from os.path import dirname, join as pjoin
-
-import nibabel as nib
 import numpy as np
 import numpy.linalg as npl
 
-import dipy
-from dipy.viz import regtools
-import numpy.linalg as npl
-from scipy.ndimage import affine_transform
 import matplotlib.pyplot as plt
 
-from fmri_utils.registration.shared import get_data_affine, decompose_rot_mat
-from fmri_utils.registration.code_our_version import resample, transform_cmass, transform_rigid, transform_affine, mutual_info
-from fmri_utils.func_preproc.rotations import x_rotmat, y_rotmat, z_rotmat
+import nibabel as nib
 
-## functions
-
-def make_pngs(static, new_img, file):
-    for i in range(3):
-        regtools.overlay_slices(static, new_img, None, i, "Static", "Moving", file+"_"+str(i)+".png")
-    plt.close("all")
+from fmri_utils.registration.shared import get_data_affine
+from fmri_utils.registration.code_our_version import params2affine, resample, affine_registration, generate_transformed_images, neg_mutual_info
 
 
-## load data
-print('load data\n')
-template = nib.load('../../../data/mni_icbm152_t1_tal_nlin_asym_09a_brain.nii.gz')
+### transform MNI template by known translations & rotations
 
-static = template.get_data()
-static_affine = template.get_affine()
+# set paths
+MY_DIR = dirname(abspath(__file__))
+project_dirname = 'project-red'
+idx = MY_DIR.rfind(project_dirname)
+project_path = MY_DIR[:(idx+len(project_dirname))]
 
-moving = template.get_data()
-moving_affine = template.get_affine()
+figs_dir = pjoin(project_path, 'report', 'figures')
+template_file = pjoin(project_path, 'data', 'registration_example_files', 'mni_icbm152_t1_tal_nlin_asym_09a_brain.nii.gz')
+resampled_file = pjoin(figs_dir, 'mni_icbm152_t1_tal_nlin_asym_09a_brain_changed.nii.gz')
 
+file_prefix = resampled_file.split('/')[-1]
+idx = file_prefix.find('.nii')
+file_prefix = file_prefix[:idx]
 
-## set initial transform T
-print('set initial transform\n')
-rot_init = [0.2, -0.2, 0.5]
-trans_init = [59,-3,-20]
-rot_mat = z_rotmat(rot_init[2]).dot(y_rotmat(rot_init[1])).dot(x_rotmat(rot_init[0]))
-SHIFT_affine = nib.affines.from_matvec(rot_mat, trans_init)
-#SHIFT_affine = np.eye(4)
+summary_filename = pjoin(figs_dir, file_prefix + '_generated_images_summary.csv')
 
-## set downsampling of data (while working to increase speed)
-print('set data downsample\n')
-SCALE = 0.5
-SCALE_affine = nib.affines.from_matvec(np.diagflat([1/SCALE]*3), np.zeros(3))
+# set transformation (translation & rotation only)
+trans_init = [59, -3, -20] # voxels in x, y, z
+rot_init = [0.2, -0.2, 0.5] # radians around x, y, z
 
-static_scaled_shape = (np.array(static.shape)*SCALE).astype('int')
-moving_scaled_shape = (np.array(moving.shape)*SCALE).astype('int')
+# load template
+template_data, template_affine = get_data_affine(template_file)
 
+# apply transformation
+change_affine = params2affine(trans_init+rot_init)
+resampled_data = resample(template_data, template_data, template_affine, template_affine.dot(change_affine))
 
-## generate starting "static" and "moving" images
-static_scaled_affine = static_affine.dot(SCALE_affine)
-moving_scaled_affine = moving_affine.dot(SCALE_affine)
+# save resample dimage
+img = nib.Nifti1Image(resampled_data, template_affine)
+nib.save(img, resampled_file)
 
-static_scaled = resample(np.zeros(static_scaled_shape), static, static_scaled_affine, static_affine)
-moving_scaled = resample(np.zeros(moving_scaled_shape), moving, moving_scaled_affine, moving_affine.dot(SHIFT_affine))
+### do affine registration for template and transformed image
+SCALE = 1
+affine_registration(template_file, resampled_file, 1, figs_dir, 10)
+generate_transformed_images(template_file, resampled_file, 1, figs_dir, figs_dir, 'png')
 
-static = static_scaled
-static_affine = static_scaled_affine
+### show MI for each step
+labels = ['identical', 'perfect\ninverse', 'trans-\nformed', 'cmass', 'trans-\nlation', 'rigid', 'full\naffine']
+neg_MI = [None]*len(labels)
 
-moving = moving_scaled
-moving_affine = moving_scaled_affine
+# get mutual info for template and itself (identical)
+neg_MI[0] = neg_mutual_info(template_data, template_data)
 
+# get mutual info for template and perfect inverse transform of transfromed image
+resampled_data, resampled_affine = get_data_affine(resampled_file)
+returned_data = resample(resampled_data, resampled_data, resampled_affine, resampled_affine.dot(npl.inv(change_affine)))
+neg_MI[1] = neg_mutual_info(template_data, returned_data)
 
-## get max MI possible with these images
-print('MAX MI for static & static:')
-print('...MI = '+str(mutual_info(static, static, 32))+'\n')
+# load registration step MI values from image summary csv
+summary_file = open(summary_filename, 'rU')
+summary_dict = {}
+for line in csv.reader(summary_file):
+    if line[2].find(file_prefix)>-1:
+        transform = line[2].split('_')[-1].replace('.txt','')
+        summary_dict[transform] = float(line[3])
+summary_file.close()
 
-mat,vec = nib.affines.to_matvec(npl.inv(SHIFT_affine))
-fix_affine = nib.affines.from_matvec(mat,vec/2)
-moving_returned = resample(static, moving, static_affine, moving_affine.dot(fix_affine))
+neg_MI[2] = summary_dict['resampled']
+neg_MI[3] = summary_dict['cmass']
+neg_MI[4] = summary_dict['translation']
+neg_MI[5] = summary_dict['rigid']
+neg_MI[6] = summary_dict['sheared']
 
-print('MAX MI for static & perfect (moving transformed back):')
-print('...MI = '+str(mutual_info(static, moving_returned, 32))+'\n')
+# make fig
+fig, ax = plt.subplots(1,1)
 
-make_pngs(static, moving_returned, "_perfect")
+ax.bar(range(7),np.array(neg_MI), align = 'center', color = ['red','green']+ ['blue']*5)
+ax.plot([-1, 7], [neg_MI[1], neg_MI[1]], 'k--')
 
+ax.set_xticklabels(['']+labels, ha = 'center')
+ax.set_ylabel('- MI')
 
-## resample into template space
-print('resample into static space (A_resampled*.png)')
-resampled = resample(static, moving, static_affine, moving_affine)
+for i, xpos in enumerate(ax.get_xticks()[1:-1]):
+    ax.text(xpos,-1.7, '('+str(round(neg_MI[i],3))+')', size = 10,  ha = 'center')
+fig.subplots_adjust(bottom=0.2)
 
-make_pngs(static, resampled, "A_resampled")
+ax.set_title('negative mutual information, between template and *')
 
-print('... MI = '+str(mutual_info(static, resampled, 32))+'\n')
-
-
-## center of mass transform
-print('do center of mass transform (B_cmass*.png)')
-cmass_affine = transform_cmass(static, moving, static_affine, moving_affine)
-updated_moving_affine = cmass_affine.dot(moving_affine)
-cmass = resample(static, moving, static_affine, updated_moving_affine)
-
-make_pngs(static, cmass, "B_cmass")
-
-print('... MI = '+str(mutual_info(static, cmass, 32))+'\n')
-
-
-## rigid: translation only
-print('do translation transfrom (C_translation*.png)')
-translation_affine = transform_rigid(static, moving, static_affine, moving_affine, cmass_affine, 10, "translations")
-updated_moving_affine = translation_affine.dot(moving_affine)
-translation = resample(static, moving, static_affine, updated_moving_affine)
-
-make_pngs(static, translation, "C_translation")
-
-print('... MI = '+str(mutual_info(static, translation, 32))+'\n')
-
-
-## rigid: translation and rotation
-print('do rigid transform (D_rigid*.png)')
-rigid_affine = transform_rigid(static, moving, static_affine, moving_affine, translation_affine, 10, "all")
-updated_moving_affine = rigid_affine.dot(moving_affine)
-rigid = resample(static, moving, static_affine, updated_moving_affine)
-
-make_pngs(static, rigid, "D_rigid")
-
-print('... MI = '+str(mutual_info(static, rigid, 32))+'\n')
-
-
-## affine: translation, rotation, scaling, and shearing
-print('do full affine transform (E_affine*.png)')
-shearing_affine = transform_affine(static, moving, static_affine, moving_affine, rigid_affine, 10, "all")
-updated_moving_affine = shearing_affine.dot(moving_affine)
-sheared = resample(static, moving, static_affine, updated_moving_affine)
-
-make_pngs(static, sheared, "E_affine")
-
-print('... MI = '+str(mutual_info(static, sheared, 32)))
+plt.show()
+fig.savefig(resampled_file.replace('.nii.gz', '_MI.png'))
